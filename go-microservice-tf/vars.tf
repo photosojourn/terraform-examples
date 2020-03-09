50201c156176db3b96722f2a19954e7b4579300d
Russ Whelan <russell.whelan@gmail.com>
2020-03-09 14:57:28 +0000

    Fix vars

--- go-microservice-tf/vars.tf
+++ go-microservice-tf/vars.tf
@@ -1,7 +1,7 @@
-/*variable "deploy_image" {
+variable "deploy_image" {
   description = "The URI of the image used for the Helm deploy."
   type        = "string"
-}*/
+}
 
 variable "github_token" {
   description = "The Github token to allow CodePipeline to access the code."
